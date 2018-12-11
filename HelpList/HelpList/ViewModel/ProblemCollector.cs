e1961e7ac1b94f7f1852f85ed362169df87a5d7d
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2018-12-11 13:13:32 +0100

    marks ændringer 11/12 2018 Admin - oprettelse og slettelse af henholdsvis klasser, studerende og lærere.

--- HelpList/HelpList/ViewModel/ProblemCollector.cs
+++ HelpList/HelpList/ViewModel/ProblemCollector.cs
@@ -13,13 +13,15 @@ namespace HelpList.ViewModel
         #region PropertyChangeSupport
         public event PropertyChangedEventHandler PropertyChanged;
 
+
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
 
-        //Instance Field
+        #region Instance Field
+
         private ObservableCollection<Problem> _problems;
         private string _name;
         private string _topic;
@@ -27,9 +29,10 @@ namespace HelpList.ViewModel
         private string _description;
         private DateTime _date;
         private Problem _selectedProblem;
+        #endregion
 
+        #region Constructor
 
-        //Constructor
         public ProblemCollector()
         {
             _problems = new ObservableCollection<Problem>();
@@ -41,9 +44,9 @@ namespace HelpList.ViewModel
             RemoveCommand = new RelayCommand(Remove);
             RemoveAllCommand = new RelayCommand(RemoveAll);
         }
+        #endregion
 
-
-        //Property
+        #region Property
 
         public ObservableCollection<Problem> Problems
         {
@@ -94,9 +97,10 @@ namespace HelpList.ViewModel
         public RelayCommand AddCommand { get; set; }
         public RelayCommand RemoveCommand { get; set; }
         public RelayCommand RemoveAllCommand { get; set; }
+        #endregion
 
+        #region Methods
 
-        //Methods
         public void Add()
         {
             _problems.Add(new Problem(Name, Topic, Location, Description));
@@ -111,25 +115,12 @@ namespace HelpList.ViewModel
                 OnPropertyChanged();
             }
         }
+
         public void RemoveAll()
         {
             _problems.Clear();
             OnPropertyChanged();
         }
-
-        //public void RemoveJustOne()
-        //{
-        //    if (SelectedProblem != null)
-        //    {
-        //        if (Name==Name)
-        //        {
-        //            _problems.Remove(_selectedProblem);
-        //        OnPropertyChanged();
-        //        }
-
-        //    }
-        //}
-
-
+        #endregion
     }
 }
